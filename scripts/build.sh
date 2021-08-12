028e37c8acbe1429c4697b32301b4d354c30b948
MFRoy <mroy@qa.com>
2021-08-12 16:02:54 +0000

    jenkinsfile updated

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker login -u &(DOCKERHUUB_CREDENTIALS_USR) -p &(DOCKERHUUB_CREDENTIALS_PSW)
+docker login -u $(DOCKERHUUB_CREDENTIALS_USR) -p $(DOCKERHUUB_CREDENTIALS_PSW)
 docker-compose build --parallel && \
 docker-compose push
