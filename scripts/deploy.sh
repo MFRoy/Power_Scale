0eb6c24f8c7355dc91a9ddc4c4c5c5b5fcc080ca
MFRoy <mroy@qa.com>
2021-08-13 10:17:29 +0000

    jenkins test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,4 @@
 
 scp $WORKSPACE/docker-compose.yaml jenkins@swarm-master
 
-ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-master "export DATABASE_URI=$DATABASE_URI && docker stack deploy --compose-file docker-compose.yaml project1"<< EOF
-docker stack deploy --compose-file docker-compose.yaml project1
-EOF
+ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-master "export DATABASE_URI=$DATABASE_URI && docker stack deploy --compose-file docker-compose.yaml project1"
